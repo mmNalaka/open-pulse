f5f77bcadd4345e8e95bbaa12d21cca7a4d8e900
Nalaka Manathunga <nalaka.manathunga@gmail.com>
2025-08-27 19:49:03 +0200

    refactor: restructure monorepo with new app and service packages

---
+++ @service-core/src/lib/auth.ts
@@ -0,0 +1,24 @@
+/** biome-ignore-all lint/performance/noNamespaceImport: no treeshaking needed for schema */
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { db } from '../db';
+import * as schema from '../db/schema/auth';
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: 'pg',
+
+    schema,
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || ''],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true,
+    },
+  },
+});
